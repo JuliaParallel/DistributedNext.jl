7fd85d78f8417bc56ce04421a6beb17063d72917
JamesWrigley <james@puiterwijk.org>
2024-11-02 00:06:11 +0100

    fixup! debug

--- src/process_messages.jl
+++ src/process_messages.jl
@@ -206,7 +206,7 @@ function message_handler_loop(r_stream::IO, w_stream::IO, incoming::Bool)
             end
             readbytes!(r_stream, boundary, length(MSG_BOUNDARY))
 
-            # println("got msg: ", typeof(msg))
+            println("got msg: ", typeof(msg))
             handle_msg(msg, header, r_stream, w_stream, version)
         end
     catch e
