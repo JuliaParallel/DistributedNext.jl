6f957b2428013887a4a222e183df05cd4162ff76
JamesWrigley <james@puiterwijk.org>
2026-03-21 14:41:23 +0100

    Add more precompilation statements
    This lowers the time for `addprocs(1)` from ~3.1s to ~1.8s on Julia 1.12.

--- src/precompile.jl
+++ src/precompile.jl
@@ -3,6 +3,13 @@ precompile(Tuple{typeof(DistributedNext.procs)})
 precompile(Tuple{typeof(DistributedNext.finalize_ref), DistributedNext.Future})
 precompile(Tuple{typeof(DistributedNext.setup_launched_worker), DistributedNext.LocalManager, DistributedNext.WorkerConfig, Vector{Int}})
 precompile(Tuple{typeof(DistributedNext.process_tcp_streams), Sockets.TCPSocket, Sockets.TCPSocket, Bool})
+precompile(Tuple{typeof(Serialization.serialize), DistributedNext.ClusterSerializer{Sockets.TCPSocket}, Array{Any, 1}})
+precompile(Tuple{typeof(DistributedNext.parse_connection_info), String})
+precompile(Tuple{typeof(DistributedNext._run_callbacks_concurrently), String, Base.Dict{Any, Union{Function, Type}}, Int64, Array{Tuple{DistributedNext.LocalManager, Base.Dict{Symbol, Any}}, 1}})
+precompile(Tuple{typeof(DistributedNext._run_callbacks_concurrently), String, Base.Dict{Any, Union{Function, Type}}, Int64, Array{Int64, 1}})
+precompile(Tuple{typeof(DistributedNext.read_worker_host_port), Base.PipeEndpoint})
+precompile(Tuple{typeof(Base.put!), Base.Channel{Any}, Int64})
+precompile(Tuple{typeof(Base.put!), Base.Channel{Any}, WeakRef})
 
 precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.IdentifySocketMsg, DistributedNext.MsgHeader, Sockets.TCPSocket, Sockets.TCPSocket, VersionNumber})
 precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.CallWaitMsg, DistributedNext.MsgHeader, Sockets.TCPSocket, Sockets.TCPSocket, VersionNumber})
@@ -13,6 +20,12 @@ precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.IdentifySoc
 precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.RemoteDoMsg, DistributedNext.MsgHeader, Sockets.TCPSocket, Sockets.TCPSocket, VersionNumber})
 precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.JoinPGRPMsg, DistributedNext.MsgHeader, Sockets.TCPSocket, Sockets.TCPSocket, VersionNumber})
 precompile(Tuple{typeof(DistributedNext.handle_msg), DistributedNext.JoinCompleteMsg, DistributedNext.MsgHeader, Sockets.TCPSocket, Sockets.TCPSocket, VersionNumber})
+
+# These lines were obtained from `addprocs(1; exeflags=`--trace-compile=compilation.txt --trace-compile-timing`)`
+precompile(Tuple{typeof(DistributedNext.start_worker)})
+precompile(Tuple{typeof(DistributedNext.start_worker), Base.PipeEndpoint, String})
+precompile(Tuple{typeof(DistributedNext.set_valid_processes), Array{Int64, 1}})
+precompile(Tuple{typeof(DistributedNext.terminate_all_workers)})
 
 # This is disabled because it doesn't give much benefit
 # and the code in Distributed is poorly typed causing many invalidations
